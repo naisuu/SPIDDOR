16439b0c15de289e2f91c299dfb1ca1b1f407760
naisuu <julius.cathalina@gmail.com>
2021-05-26 21:32:55 +0200

    Modify the writing of dynamic evolution func to include time-step-wise manipulation of polymorphism

--- R/write_dynamic_evolution_cpp.R
+++ R/write_dynamic_evolution_cpp.R
@@ -6,8 +6,8 @@ write.dynamic_evolution_cpp=function(arguments,fun_header,nodes.names)
 // [[Rcpp::export]]
 NumericVector time_evolution_f(const int& ts",
               arguments,",std::vector<std::string> Knockouts,std::vector<std::string> Over_expr,
-              std::vector<std::string> Over_expr_AA,
-              List MCAB_times,List Tx_times, List Polym,
+              std::vector<std::string> Over_expr_AA, std::vector<std::string> Mutations,
+              List MCAB_times,List Tx_times, List MUT_times, List Polym,
               IntegerVector Initial_cond, const bool asynchronous){
 
   std::string node_i;
@@ -17,6 +17,8 @@ NumericVector time_evolution_f(const int& ts",
   
   dyn1<-"
   const int n_nodes = sizeof(nodes_names) / sizeof(nodes_names[0]);
+  // Polymorphism constant when applying polymorphism for specific time steps only
+  const float poly_val = 0.1;
   //Pattern and update creation:
   int *pattern = new int[n_nodes*(ts + 1)]();
   int *update =new int[n_nodes*(ts + 1)]();
@@ -26,7 +28,7 @@ NumericVector time_evolution_f(const int& ts",
   for (int i = 0; i < n_nodes; i++) { update[i*(ts)+i] = ( 1 ); }\n"
   Polymorphism<-c()
   for(i in nodes.names)
-    Polymorphism<-c(Polymorphism,paste("\tconst double P_",i,'=as<double>(Polym["',i,'"]);',sep="") ) 
+    Polymorphism<-c(Polymorphism,paste("\tdouble P_",i,'=as<double>(Polym["',i,'"]);',sep="") ) 
 
 dyn2<-"\n  
   //Iterate:
@@ -42,6 +44,29 @@ dyn2<-"\n
           continue;
         }
       }
+      
+      // Check if mutations (polymorphisms) are passed as arguments
+      if (std::find(Mutations.begin(), Mutations.end(), node_i) != Mutations.end()) {
+        // If mutations were requested but the timesteps were not, this mutation applies indefinitely
+        // This is kind of redundant as this feature already existed, however this can serve as syntax sugar
+        if (MUT_times.size() == 0) {
+          Polym[node_i] = polym_val; // Set the polymorphism of that node to our preset polymorphism constant
+          continue;
+        } else {
+          // if a time step range is given, only set the polymorphism constant for those time steps.
+          int pos = std::find(Mutations.begin(), Mutations.end(), node_i) - Mutations.begin();
+          std::vector<int>MUT_time = MUT_times[pos];
+          
+          if (std::find(MUT_time.begin(), MUT_time.end(), j) != MUT_time.end()) {
+            Polym[node_i] = polym_val;
+          } else {
+            // Once the specified time steps have been completed, set the polymorphism back to default (1)
+            Polym[node_i] = 1;
+          }
+        }
+      }
+      
+      
       if (std::find(Knockouts.begin(), Knockouts.end(), node_i) != Knockouts.end()){// && MCAB_times.size()==0){
         int node = std::distance(nodes_names, std::find(nodes_names, nodes_names + (n_nodes - 1), node_i));
         if(MCAB_times.size()==0){
